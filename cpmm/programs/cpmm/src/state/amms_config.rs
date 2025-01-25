677f632d84e8d1246aae154c63a946fa751cbbf3
Vondert <25042018avic@gmail.com>
2025-01-25 21:28:36 +0200

    Documented and tested AmmsConfig and AmmsConfigsManager

--- cpmm/programs/cpmm/src/state/amms_config.rs
+++ cpmm/programs/cpmm/src/state/amms_config.rs
@@ -1,22 +1,46 @@
 use anchor_lang::{account, InitSpace};
 use anchor_lang::prelude::*;
 use crate::error::ErrorCode;
+
+/// Represents a configuration object for managing fees and authorities in AMMs.
+///
+/// This structure contains details such as fee rates, the authority responsible for fee collection,
+/// and a unique identifier to track the configuration within an `AmmsConfigsManager`.
 #[account]
 #[derive(InitSpace)]
 pub struct AmmsConfig {
-    pub bump: u8,   // 1
-    // Id of the config in AmmsConfigsManagers's configs collection
-    pub id: u64,    // 8
-    // Authority that will collect fees from pools
-    pub fee_authority: Pubkey,  // 32
-    // Providers fee rate in basis points (1 = 0.01%)
-    pub providers_fee_rate_basis_points: u16, // 2
-    // Protocol fee rate in basis points (1 = 0.01%)
-    pub protocol_fee_rate_basis_points: u16, // 2
+    /// Canonical bump seed for the account's PDA.
+    pub bump: u8,   // 1 byte
+
+    /// A unique identifier for this configuration in the `AmmsConfigsManager` collection.
+    pub id: u64,    // 8 bytes
+
+    /// The public key of the authority responsible for collecting protocol fees from pools.
+    pub fee_authority: Pubkey,  // 32 bytes
+
+    /// The fee rate for liquidity providers, measured in basis points (1 basis point = 0.01%).
+    pub providers_fee_rate_basis_points: u16, // 2 bytes
+
+    /// The protocol's fee rate, measured in basis points (1 basis point = 0.01%).
+    pub protocol_fee_rate_basis_points: u16, // 2 bytes
 }
 
 impl AmmsConfig {
+    /// The seed used to derive the account's PDA.
     pub const SEED: &'static [u8] = b"amms_config";
+
+    /// Initializes the `AmmsConfig` with the provided parameters.
+    ///
+    /// # Parameters
+    /// - `fee_authority`: The public key of the authority that will collect fees.
+    /// - `protocol_fee_rate_basis_points`: The protocol fee rate, measured in basis points (1 = 0.01%).
+    /// - `providers_fee_rate_basis_points`: The providers' fee rate, measured in basis points (1 = 0.01%).
+    /// - `id`: A unique identifier for this configuration.
+    /// - `bump`: The bump seed for the account's PDA.
+    ///
+    /// # Errors
+    /// - Returns `ErrorCode::ConfigFeeRateExceeded` if the sum of `protocol_fee_rate_basis_points`
+    ///   and `providers_fee_rate_basis_points` exceeds 10,000 (100%).
     pub(crate) fn initialize(&mut self, fee_authority: Pubkey, protocol_fee_rate_basis_points: u16, providers_fee_rate_basis_points: u16, id: u64, bump: u8) -> Result<()> {
         require!(providers_fee_rate_basis_points + protocol_fee_rate_basis_points <= 10000, ErrorCode::ConfigFeeRateExceeded);
         
@@ -28,7 +52,96 @@ impl AmmsConfig {
         
         Ok(())
     }
-    pub(crate) fn update_fee_authority(&mut self, fee_authority: Pubkey) -> () {
+
+    /// Updates the `fee_authority` field with a new authority public key.
+    ///
+    /// # Parameters
+    /// - `fee_authority`: The new public key of the authority responsible for collecting fees.
+    pub(crate) fn update_fee_authority(&mut self, fee_authority: Pubkey) {
         self.fee_authority = fee_authority;
+    }
+}
+
+#[cfg(test)]
+mod amms_config_tests {
+    use anchor_lang::Discriminator;
+    use crate::constants::ANCHOR_DISCRIMINATOR;
+    use super::*;
+
+    /// Tests the correct initialization of the `AmmsConfig` struct.
+    #[test]
+    fn test_amms_config_initialize() {
+        let mut amms_config = AmmsConfig {
+            bump: 0,
+            id: 0,
+            fee_authority: Pubkey::default(),
+            providers_fee_rate_basis_points: 0,
+            protocol_fee_rate_basis_points: 0,
+        };
+
+        let fee_authority = Pubkey::new_unique();
+        let protocol_fee_rate_basis_points = 200;
+        let providers_fee_rate_basis_points = 300;
+        let id = 42u64;
+        let bump = 42u8;
+
+        amms_config.initialize(fee_authority, protocol_fee_rate_basis_points, providers_fee_rate_basis_points, id, bump).unwrap();
+
+        assert_eq!(amms_config.bump, bump);
+        assert_eq!(amms_config.id, id);
+        assert_eq!(amms_config.fee_authority, fee_authority);
+        assert_eq!(amms_config.protocol_fee_rate_basis_points, protocol_fee_rate_basis_points);
+        assert_eq!(amms_config.providers_fee_rate_basis_points, providers_fee_rate_basis_points);
+    }
+
+
+    /// Tests the `update_fee_authority` method of the `AmmsConfig` struct.
+    #[test]
+    fn test_amms_config_update_fee_authority() {
+        let mut amms_config = AmmsConfig {
+            bump: 42,
+            id: 42,
+            fee_authority: Pubkey::default(),
+            providers_fee_rate_basis_points: 300,
+            protocol_fee_rate_basis_points: 200,
+        };
+
+        let new_fee_authority = Pubkey::new_unique();
+        amms_config.update_fee_authority(new_fee_authority);
+        assert_eq!(amms_config.fee_authority, new_fee_authority);
+    }
+
+    /// Tests `AmmsConfig` account data layout.
+    #[test]
+    fn test_amms_config_data_layout() {
+        let fee_authority = Pubkey::new_unique();
+        let bump = 42u8;
+        let id = 42u64;
+        let providers_fee_rate_basis_points: u16 = 200;
+        let protocol_fee_rate_basis_points: u16 = 300;
+
+        let mut data = [0u8; ANCHOR_DISCRIMINATOR + 45];
+        let mut offset = 0;
+
+        data[offset..offset + ANCHOR_DISCRIMINATOR].copy_from_slice(&AmmsConfig::discriminator()); offset += ANCHOR_DISCRIMINATOR;
+        data[offset..offset + 1].copy_from_slice(&bump.to_le_bytes()); offset += 1;
+        data[offset..offset + 8].copy_from_slice(&id.to_le_bytes()); offset += 8;
+        data[offset..offset + 32].copy_from_slice(fee_authority.as_ref()); offset += 32;
+        data[offset..offset + 2].copy_from_slice(&providers_fee_rate_basis_points.to_le_bytes()); offset += 2;
+        data[offset..offset + 2].copy_from_slice(&protocol_fee_rate_basis_points.to_le_bytes()); offset += 2;
+
+        assert_eq!(ANCHOR_DISCRIMINATOR + AmmsConfig::INIT_SPACE, offset);
+        
+        let deserialized_amms_config = AmmsConfig::try_deserialize(&mut data.as_slice()).unwrap();
+
+        assert_eq!(deserialized_amms_config.bump, bump);
+        assert_eq!(deserialized_amms_config.id, id);
+        assert_eq!(deserialized_amms_config.fee_authority, fee_authority);
+        assert_eq!(deserialized_amms_config.providers_fee_rate_basis_points, providers_fee_rate_basis_points);
+        assert_eq!(deserialized_amms_config.protocol_fee_rate_basis_points, protocol_fee_rate_basis_points);
+
+        let mut serialized_amms_config = Vec::new();
+        deserialized_amms_config.try_serialize(&mut serialized_amms_config).unwrap();
+        assert_eq!(serialized_amms_config.as_slice(), data.as_ref());
     }
 }
--- cpmm/programs/cpmm/src/state/amms_configs_manager.rs
+++ cpmm/programs/cpmm/src/state/amms_configs_manager.rs
@@ -1,31 +1,178 @@
 use anchor_lang::{account, InitSpace};
 use anchor_lang::prelude::*;
 
+/// Represents the manager for AMM configurations.
+///
+/// This account manages multiple AMM configurations and tracks the authority
+/// responsible for their governance. It also maintains a count of configurations
+/// and provides utility methods for updating key parameters.
 #[account]
 #[derive(InitSpace)]
 pub struct AmmsConfigsManager {
-    pub authority: Pubkey,  // 32
-    pub head_authority: Pubkey, // 32
-    pub configs_count: u64, // 8
-    pub bump: u8,   // 1
+    /// The public key of the authority responsible for managing configurations.
+    pub authority: Pubkey,  // 32 bytes
+
+    /// The public key of the head authority, which may have broader governance privileges.
+    pub head_authority: Pubkey, // 32 bytes
+
+    /// The number of configurations managed by this account.
+    pub configs_count: u64, // 8 bytes
+
+    /// The canonical bump seed used for the account's PDA.
+    pub bump: u8,   // 1 byte
 }
 
 impl AmmsConfigsManager {
+    /// The seed used to derive the account's PDA.
     pub const SEED: &'static [u8] = b"amms_configs_manager";
-    pub(crate) fn initialize(&mut self, authority: Pubkey, head_authority: Pubkey, bump: u8) -> () {
+
+    /// Initializes the `AmmsConfigsManager` with the provided parameters.
+    ///
+    /// # Parameters
+    /// - `authority`: The public key of the initial authority for managing configurations.
+    /// - `head_authority`: The public key of the head authority with broader governance privileges.
+    /// - `bump`: The bump seed for the account's PDA.
+    ///
+    /// # Behavior
+    /// - Sets the initial `configs_count` to 0.
+    /// - Updates the authority and head authority fields with the provided values.
+    pub(crate) fn initialize(&mut self, authority: Pubkey, head_authority: Pubkey, bump: u8) {
         self.bump = bump;
         self.configs_count = 0;
         self.update_authority(authority);
         self.update_head_authority(head_authority);
     }
 
+
+    /// Updates the `authority` field with a new public key.
+    ///
+    /// # Parameters
+    /// - `authority`: The new public key for the authority managing configurations.
     pub(crate) fn update_authority(&mut self, authority: Pubkey) {
         self.authority = authority;
     }
+
+    /// Updates the `head_authority` field with a new public key.
+    ///
+    /// # Parameters
+    /// - `head_authority`: The new public key for the head authority with broader governance privileges.
     pub(crate) fn update_head_authority(&mut self, head_authority: Pubkey) {
         self.head_authority = head_authority;
     }
-    pub(crate) fn increment_configs_count(&mut self) -> () {
+
+    /// Increments the `configs_count` field by 1.
+    ///
+    /// # Behavior
+    /// - Safely increments the count of configurations, ensuring no overflow occurs.
+    /// - Panics if the increment operation fails (e.g., due to an overflow).
+    pub(crate) fn increment_configs_count(&mut self) {
         self.configs_count = self.configs_count.checked_add(1).unwrap()
+    }
+}
+
+#[cfg(test)]
+mod amms_configs_manager_tests {
+    use anchor_lang::Discriminator;
+    use crate::constants::ANCHOR_DISCRIMINATOR;
+    use super::*;
+
+    /// Tests the correct initialization of the `AmmsConfigsManager` struct.
+    #[test]
+    fn test_amms_configs_manager_initialize() {
+        let mut manager = AmmsConfigsManager {
+            authority: Pubkey::default(),
+            head_authority: Pubkey::default(),
+            configs_count: 0,
+            bump: 0,
+        };
+
+        let authority = Pubkey::new_unique();
+        let head_authority = Pubkey::new_unique();
+        let bump = 42u8;
+
+        manager.initialize(authority, head_authority, bump);
+
+        assert_eq!(manager.authority, authority);
+        assert_eq!(manager.head_authority, head_authority);
+        assert_eq!(manager.configs_count, 0);
+        assert_eq!(manager.bump, bump);
+    }
+    
+    /// Tests the `update_authority` method of the `AmmsConfigsManager` struct.
+    #[test]
+    fn test_amms_configs_manager_update_authority() {
+        let mut manager = AmmsConfigsManager {
+            authority: Pubkey::default(),
+            head_authority: Pubkey::new_unique(),
+            configs_count: 10,
+            bump: 42,
+        };
+
+        let new_authority = Pubkey::new_unique();
+        manager.update_authority(new_authority);
+        
+        assert_eq!(manager.authority, new_authority);
+    }
+
+    /// Tests the `update_head_authority` method of the `AmmsConfigsManager` struct.
+    #[test]
+    fn test_amms_configs_manager_update_head_authority(){
+        let mut manager = AmmsConfigsManager {
+            authority: Pubkey::new_unique(),
+            head_authority: Pubkey::default(),
+            configs_count: 10,
+            bump: 42,
+        };
+
+        let new_head_authority = Pubkey::new_unique();
+        manager.update_head_authority(new_head_authority);
+
+        assert_eq!(manager.head_authority, new_head_authority);
+    }
+
+    /// Tests the `increment_configs_count` method of the `AmmsConfigsManager` struct.
+    #[test]
+    fn test_amms_configs_manager_increment_configs_count(){
+        let mut manager = AmmsConfigsManager {
+            authority: Pubkey::new_unique(),
+            head_authority: Pubkey::new_unique(),
+            configs_count: 5,
+            bump: 42,
+        };
+
+        manager.increment_configs_count();
+
+        assert_eq!(manager.configs_count, 6);
+    }
+    
+    /// Tests the `update_head_authority` method of the `AmmsConfigsManager` struct.
+    #[test]
+    fn test_amms_configs_manager_data_layout() {
+        let authority = Pubkey::new_unique();
+        let head_authority = Pubkey::new_unique();
+        let configs_count = 42u64;
+        let bump = 42u8;
+
+        let mut data = [0u8; ANCHOR_DISCRIMINATOR + 73];
+        let mut offset = 0;
+
+        data[offset..offset + ANCHOR_DISCRIMINATOR].copy_from_slice(&AmmsConfigsManager::discriminator()); offset += ANCHOR_DISCRIMINATOR;
+        data[offset..offset + 32].copy_from_slice(authority.as_ref()); offset += 32;
+        data[offset..offset + 32].copy_from_slice(head_authority.as_ref()); offset += 32;
+        data[offset..offset + 8].copy_from_slice(&configs_count.to_le_bytes()); offset += 8;
+        data[offset] = bump; offset += 1;
+
+        assert_eq!(offset, ANCHOR_DISCRIMINATOR + 73);
+        
+        let deserialized_manager = AmmsConfigsManager::try_deserialize(&mut data.as_ref()).unwrap();
+
+        assert_eq!(deserialized_manager.authority, authority);
+        assert_eq!(deserialized_manager.head_authority, head_authority);
+        assert_eq!(deserialized_manager.configs_count, configs_count);
+        assert_eq!(deserialized_manager.bump, bump);
+
+        let mut serialized_data = Vec::new();
+        deserialized_manager.try_serialize(&mut serialized_data).unwrap();
+        assert_eq!(serialized_data.as_slice(), data.as_ref());
     }
 }
